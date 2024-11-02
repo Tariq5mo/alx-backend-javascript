2d1f898c150394c21dac147092db5427949ff8bf
Tariq5mo <tariqomer756@gmail.com>
2024-11-02 16:04:13 +0200

    Add task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,5 +3,13 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((result) => result);
+    .then((results) => {
+      const response = results.map((result) => {
+        if (result.status === 'fulfilled') {
+          return { status: 'fulfilled', value: result.value };
+        }
+        return { status: 'rejected', reason: result.reason };
+      });
+      return response;
+    });
 }
