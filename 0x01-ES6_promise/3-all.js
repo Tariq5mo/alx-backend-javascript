cd581417b3db739aaf537d5711875c18b2dfc942
Tariq5mo <tariqomer756@gmail.com>
2024-11-02 14:58:51 +0200

    Fix task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,10 +4,8 @@ export default function handleProfileSignup() {
   const up = uploadPhoto();
   const userp = createUser();
   Promise.all([up, userp])
-    .then((result) => {
-      const photo = result[0];
-      const name = result[1];
-      console.log(photo.body, name.firstName, name.lastName);
+    .then(([photo, name]) => {
+      console.log(`${photo.body} ${name.firstName} ${name.lastName}`);
     })
     .catch(() => console.log('Signup system offline'));
 }
