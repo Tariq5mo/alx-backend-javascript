e2de9d81c170fc8b02af124357c884f83431df0d
Tariq5mo <tariqomer756@gmail.com>
2024-11-11 15:29:57 +0200

    Fix task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,14 @@
 export default function cleanSet(set, startString) {
+  if (startString === '') {
+    return Array.from(set).join('-');
+  }
   const cst = [];
   if (set !== undefined && set.constructor === Set && typeof startString === 'string') {
     for (const element of set) {
-      if (element.startsWith(startString) && startString !== '') {
+      if (element.startsWith(startString)) {
         cst.push(element.replace(startString, ''));
       }
     }
-    return String(cst.join('-'));
   }
-  return '';
+  return cst.join('-');
 }
