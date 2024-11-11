489d19890d660948861fb130a285755d0ecdf603
Tariq5mo <tariqomer756@gmail.com>
2024-11-11 15:10:49 +0200

    Fix task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(set, startString) {
   const cst = [];
-  if (set.constructor === Set && typeof startString === 'string') {
+  if (set !== undefined && set.constructor === Set && typeof startString === 'string') {
     for (const element of set) {
       if (element.startsWith(startString) && startString !== '') {
         cst.push(element.replace(startString, ''));
