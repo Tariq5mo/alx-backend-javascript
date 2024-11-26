d5dcc7b12059f12fa725aec943807e548347e0a1
Tariq5mo <tariqomer756@gmail.com>
2024-11-26 09:35:35 +0200

    Fix task 0

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,4 +1,13 @@
+/**
+ * Prints the given argument to STDOUT.
+ * @param {any} argument - The argument to be printed.
+ */
 function displayMessage(argument) {
-  process.stdout.write(`${String(argument)}\n`);
+  if (argument === null || argument === undefined) {
+    process.stdout.write('\n');
+  } else {
+    process.stdout.write(`${String(argument)}\n`);
+  }
 }
+
 module.exports = displayMessage;
