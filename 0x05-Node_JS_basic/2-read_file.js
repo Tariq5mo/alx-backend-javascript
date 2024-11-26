3552aefda25c56aab19438a6fd862abfab38808d
Tariq5mo <tariqomer756@gmail.com>
2024-11-26 15:42:12 +0200

    Add task 2

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+    const records = data.split('\n');
+
+    // Remove the last elements of the array if they are empty
+    for (let i = records.length - 1; i > 0; i -= 1) {
+      if (records[i] === '') {
+        records.pop();
+      }
+    }
+
+    const students = records.map((record) => record.split(','));
+
+    const studentsObject = students.map(([firstName, lastName, age, field]) => {
+      const studentObject = {};
+      studentObject.firstNames = firstName;
+      studentObject.lastName = lastName;
+      studentObject.age = age;
+      studentObject.field = field;
+      return studentObject;
+    });
+
+    studentsObject.shift();
+    const fieldMap = {};
+    studentsObject.forEach((student) => {
+      if (!fieldMap[student.field]) {
+        fieldMap[student.field] = [];
+      }
+      fieldMap[student.field].push(student.firstNames);
+    });
+    const output = [];
+    output.push(`Number of students: ${studentsObject.length}`);
+    for (const field of Object.keys(fieldMap)) {
+      output.push(`Number of students in ${field}: ${fieldMap[field].length}. List: ${fieldMap[field].join(', ')}`);
+    }
+    console.log(output.join('\n'));
+  } catch (err) {
+    console.error('Cannot load the database');
+    console.error(err);
+  }
+}
+
+module.exports = countStudents;
--- 0x05-Node_JS_basic/test.js
+++ 0x05-Node_JS_basic/test.js
@@ -1,14 +1,1 @@
-// Set the encoding to 'utf8' to receive string data
-process.stdin.setEncoding('utf8');
-
-// Listen for data event to read input
-process.stdin.on('data', (data) => {
-  console.log(`You entered: ${data}`);
-});
-
-// Listen for end event to know when the input stream has ended
-process.stdin.on('end', () => {
-  console.log('Input stream ended');
-});
-
-console.log('Please enter some text:');
+const fs = require('fs');
