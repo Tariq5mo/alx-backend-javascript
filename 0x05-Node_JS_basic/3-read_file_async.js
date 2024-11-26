f513e95357e68e0144b638611c37563cb9a9233e
Tariq5mo <tariqomer756@gmail.com>
2024-11-27 00:04:07 +0200

    Fix task 3

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,8 +1,8 @@
-const fs = require('fs');
+const { readFile } = require('fs');
 
 async function countStudents(path) {
   return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf8', (err, data) => {
+    readFile(path, 'utf8', (err, data) => {
       if (err) {
         reject(new Error('Cannot load the database'));
         return;
